7d1aabb7ecd052cf506175beda911c6487062ec1
Ved Tod Teckos <Ved.Teckos@proton.me>
2025-01-09 13:56:18 +0200

    feat: Add support for managing branches in the GitHub API wrapper

---
+++ src/main/java/com/simplegithub/BranchHandler.java
@@ -0,0 +1,64 @@
+package com.simplegithub;
+
+import lombok.RequiredArgsConstructor;
+import org.kohsuke.github.GHBranch;
+import org.kohsuke.github.GHRepository;
+
+import java.io.IOException;
+
+/**
+ * Handles operations related to a specific branch in a GitHub repository.
+ */
+@RequiredArgsConstructor
+public class BranchHandler {
+    private final GHRepository repository;
+    private final String branchName;
+
+    /**
+     * Gets the SHA-1 of the branch's HEAD.
+     *
+     * @return SHA-1 of the branch's HEAD
+     * @throws IOException if the branch cannot be accessed
+     */
+    public String getSha() throws IOException {
+        return getBranch().getSHA1();
+    }
+
+    /**
+     * Checks if this branch is protected.
+     *
+     * @return true if the branch is protected
+     * @throws IOException if the branch cannot be accessed
+     */
+    public boolean isProtected() throws IOException {
+        return getBranch().isProtected();
+    }
+
+    /**
+     * Gets the name of the branch.
+     *
+     * @return branch name
+     */
+    public String getName() {
+        return branchName;
+    }
+
+    /**
+     * Deletes this branch.
+     *
+     * @throws IOException if the branch cannot be deleted
+     */
+    public void delete() throws IOException {
+        getBranch().delete();
+    }
+
+    /**
+     * Gets the underlying GHBranch object.
+     *
+     * @return GHBranch object
+     * @throws IOException if the branch cannot be accessed
+     */
+    private GHBranch getBranch() throws IOException {
+        return repository.getBranch(branchName);
+    }
+} 
